979d3d89cdd2e76fa36218b47a97ad8651a1372f
monst.backup <backupmonst981183414@gmail.com>
2021-11-01 11:18:22 +0000

    test

--- team.rb
+++ team.rb
@@ -14,14 +14,14 @@ class Team
   end
 
   def show_team_result
-    puts "#{self.name}の2020年の成績は#{self.win}勝#{self.lose}敗#{self.draw}分、勝率は#{self.calc_win_rate}です。"
+    puts "#{self.name} の2020年の成績は #{self.win}勝 #{self.lose}敗 #{self.draw}分、勝率は #{self.calc_win_rate}です。"
   end
 end
 
 giants = Team.new("Giants", 67, 45, 8)
 tigers = Team.new("Tigers", 60, 53, 7)
 dragons = Team.new("Dragons", 60, 55, 5)
-baystars = Team.new("Baystars", 56, 58, 6)
+baystars = Team.new("BayStars", 56, 58, 6)
 carp = Team.new("Carp", 52, 56, 12)
 swallows = Team.new("Swallows", 41, 69, 10)
 
